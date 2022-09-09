6249649cdf0d6d2afb397ff008869dd0c1c6f4d7
Perocode <ogunromeo@gmail.com>
2022-09-08 19:30:08 -0700

    Task 04 puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  * 
  * Return: Always 0
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle \n");
+  puts("Programming is like building a multilingual puzzle\n");
   return (0);
 }

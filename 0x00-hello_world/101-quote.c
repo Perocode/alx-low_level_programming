e75e4b9b54fd8ae8874f57612d6c1cbec60add31
Perocode <ogunromeo@gmail.com>
2022-09-08 21:55:47 -0700

    Task 08 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return 1 successful
+ */
+
+int main(void)
+{
+printf("and that piece of art is usefule\" - dora Korpar, 2015-10-19\n");
+return (1);
+}

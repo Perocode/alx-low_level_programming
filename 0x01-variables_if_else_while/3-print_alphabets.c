21adf4f91d0d75f7ed92b46cc082817fec25ee29
Perocode <ogunromeo@gmail.com>
2022-09-11 14:48:03 -0700

    Task 03 lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+  char al = 'a';
+  char alp = 'A';
+
+  while (al <= 'z')
+    {
+      putchar(al);
+      al++;
+    }
+  while (alp <= 'Z')
+    {
+      putchar(alp);
+      alp++;
+    }
+  putchar('\n');
+  return (0);
+}

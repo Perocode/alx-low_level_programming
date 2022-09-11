55348850dff9f6f081ea0c2fc82e82bd0ce48443
Perocode <ogunromeo@gmail.com>
2022-09-11 13:58:50 -0700

    Task 01 last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_dt);
 }
 }
-if (l_dt > 5)                                                                                           
+if (l_dt > 5)
 {                                                                                                           
 printf("Last digit of %d is %d and is greater than 5\n", n, l_dt);
 } 

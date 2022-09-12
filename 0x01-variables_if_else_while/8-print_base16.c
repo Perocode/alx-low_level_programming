20743c860226b6da18d84ec471761e874549925a
Perocode <ogunromeo@gmail.com>
2022-09-11 21:43:12 -0700

    Task 08 base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+int ba = 0;
+char bas = 'A';
+
+while (ba <= 10)
+{
+putchar(ba%10 + '0');
+ba++;
+}
+while (bas <= 'F')
+{
+putchar(bas);
+bas++;
+}
+putchar('\n');
+return (0);
+}

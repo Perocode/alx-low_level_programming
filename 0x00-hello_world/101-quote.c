00b7951004f352f7cdac28eba195f6374d3b3cf2
Perocode <ogunromeo@gmail.com>
2022-09-12 14:31:16 -0700

    Task 101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 /**
  * main - Entry point
+ * Description:  using a write function to print a quote
  * Returns  1 (Successful)
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }

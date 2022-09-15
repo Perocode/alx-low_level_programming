9b26d0097c3dd5f6abc2e4435438114f0b4c8795
Perocode <ogunromeo@gmail.com>
2022-09-14 21:48:54 -0700

    Task 06 absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - computes the absolute value of an integer
  *  @n: the integer that helps check 
- * Returns the absolute value
+ * Returns the absolute value _abs
  */
 
 int _abs(int n)

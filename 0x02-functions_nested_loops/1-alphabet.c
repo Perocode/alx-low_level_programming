14b30f1a51b1cf34597d5a5dd1a1e41777fdc2ca
Perocode <ogunromeo@gmail.com>
2022-09-14 20:25:16 -0700

    Task 01 print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets from a-z
+ * Return: Always 0 (Successful)
+ */
+
+void print_alphabet(void)
+{
+char x = 'a';
+while(x<='z')
+{
+_putchar(x);
+x++;
+}
+_putchar('_');
+}

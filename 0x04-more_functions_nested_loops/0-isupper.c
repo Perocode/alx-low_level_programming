044c9f8a2605661a6cbae6ccfaeefba21483f207
Perocode <ogunromeo@gmail.com>
2022-09-16 14:18:05 -0700

    Task 0 uppercase check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase alphabets
+ * @c: checks and prints 1 or 0
+ * Return: either 1 or 0 
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}

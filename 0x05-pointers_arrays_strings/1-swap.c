d9c764cef5c69d4a93602e60ec832c33bd81abdb
Perocode <ogunromeo@gmail.com>
2022-09-19 19:10:45 -0700

    Task 01 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap - it helps to swap the values of the pointers
+ * @a: first pointer value
+ * @b: second pointer value
+ */
+void swap_int(int *a, int *b)
+{
+int swp;
+swp = *a;
+
+*a = *b;
+*b = swp;
+}

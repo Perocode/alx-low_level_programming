8b39de6d1e13f6cc7a993bebf356ea35701433dd
Perocode <ogunromeo@gmail.com>
2022-09-20 20:04:49 -0700

    Task 06 prints characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters of the string
+ * @str: pointer of the string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int x;
+
+for (x = 0; (*(str + x) != '\0'); x++)
+{
+if (x % 2 == 0)
+{
+_putchar(*(str + x));
+}
+x++;
+}
+_putchar('\n');
+}

5fe0849b0a2bfb8c3160aa4121c86987eca2a170
Perocode <ogunromeo@gmail.com>
2022-09-22 20:58:23 -0700

    Task 01 string concatenation with a check

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat -helps to concatenate two strings
+ * @dest: first string
+ * @src: second string
+ * @n:
+ * Return: pointer is returned
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int a = 0, b = 0;
+
+while (dest[a] != '\0')
+{
+a++;
+}
+while (src[b] != '\0' &&  b < n)
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}

24242fa6d36d726670294b6b4641a9175ae5ce8e
Perocode <ogunromeo@gmail.com>
2022-09-26 21:33:31 -0700

    Task 0 fill memory with a contstant

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - it helps fill the memory with constant
+ * @s: char pointer to string
+ * @b: constant byte
+ * @n: maximum bytes to use
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+for (x = 0; x < n; x++)
+{
+s[x] = b;
+}
+return (s);
+}

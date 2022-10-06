9764698b8edca12d3c194a1f22d6691af371dd98
Perocode <ogunromeo@gmail.com>
2022-10-05 21:23:46 -0700

    Task 0 A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: count
+ * @argv: vector
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf ("%s\n", argv[0]);
+return (0);
+}

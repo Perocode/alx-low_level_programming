cc4d0a259f165fe23ebb6c55694a09a4a90eadce
Perocode <ogunromeo@gmail.com>
2022-10-25 13:40:32 -0700

    frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: points to the elements of a list
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *tmp;
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+}
